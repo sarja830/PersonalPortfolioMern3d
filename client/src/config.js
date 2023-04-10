b1e4ae7f26b2381e4a69629042b77f7e4d0cd999
sarth830 <sarthjain830@gmail.com>
2023-04-09 20:30:34 -0400

    removed .env from frontent and created optimized build

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 // require('dotenv').config();
-export const API = process.env.REACT_APP_API_URL;
-// export const API = "http://localhost:8000/api";
+// export const API = process.env.REACT_APP_API_URL;
+export const API = "http://localhost:1337/api";
