d1649907039eaf28f40566b032a515e3027a7478
sarth830 <sarthjain830@gmail.com>
2023-04-09 20:13:29 -0400

    added some more production changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const expressValidator = require('express-validator');
-
+require('dotenv').config();
 
 const emailRoutes = require('./routes/email');
 
@@ -29,12 +29,20 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 //app.use(expressValidator());
 app.use(cors());
-
+app.use(express.static('client/dist'))
 // routes middleware
 app.use('/api', emailRoutes);
 
 const port = process.env.PORT || 8000;
 
+
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static('client/dist'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','dist','index.html'))
+    })
+}
 
 
 app.listen(port, () => {
