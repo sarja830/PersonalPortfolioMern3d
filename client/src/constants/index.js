9a07a64b91fd8dff2c33162deba2915a6ad2fe85
sarth830 <sarthjain830@gmail.com>
2025-09-06 17:36:29 -0400

    Update contact info and Neural Shift project with YouTube demo

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -420,6 +420,7 @@ const projects = [
         // image: n,
         image: neuralshift,
 
+ub
         // source_code_link: "",
         view_link: "https://www.youtube.com/watch?v=zfBMWLddNDg"
     },
