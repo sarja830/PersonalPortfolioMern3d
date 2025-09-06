c6bb896e6533cd4117e22e2144ea50a1f6fa976a
sarth830 <sarthjain830@gmail.com>
2025-09-06 18:02:52 -0400

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // No base path
+  base: '/portfolio/', // Must match repository name for GitHub Pages
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
